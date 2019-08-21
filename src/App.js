142e57807b89c30e10239d312deb37687333c680
arodri04 <augustine.rodriguez55@gmail.com>
2019-08-21 12:30:41 -0700

    oops forgot a path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
      <header className="navbar">
-      <h1>SportsBook</h1>
+      <Link to="/"><h1>SportsBook</h1></Link>
         <nav>
             <Link to="/nfl">NFL</Link>
             <Link to="/mlb">MLB</Link>
@@ -17,7 +17,7 @@ function App() {
         </nav>
      </header>
       <main>
-        <Route exact path="/" component={NFL}/>
+        <Route exact path="/NFL" component={NFL}/>
         <Route exact path="/nba" component={NBA}/>
         <Route exact path="/mlb" component={MLB}/>
         <Route exact path="/ncaa" component={NCAA}/>
