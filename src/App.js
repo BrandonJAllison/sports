205c42f696b1c0a04aa25539a031e481e7ca23a9
brellin <cekakid@gmail.com>
2019-08-21 14:22:53 -0500

    add react router with basics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import './App.scss';
+import Login from './components/Login'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>SportsBook</h1>
-        <nav>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span>7</span>
-          <span>8</span>
-        </nav>
-      </header>
-      <main>
+    <Switch>
 
-      </main>
-    </div>
-  );
+      <Route exact path='/' component={Login} />
+
+    </Switch>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux'
-import App from './App';
-import './index.css';
-import reducer from './reducers';
+import App from './App'
+import './index.css'
+import reducer from './reducers'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
     document.getElementById('root')
-);
+)
