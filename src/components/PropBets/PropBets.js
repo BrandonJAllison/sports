a9fc5b11b9574b545989053e192d84842a199b3a
Brandon Allison <brandona8128@outlook.com>
2019-08-22 22:31:47 -0400

    adding odds div

--- src/components/PropBets/PropBets.js
+++ src/components/PropBets/PropBets.js
@@ -31,14 +31,24 @@ export const PropBets = props => {
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <span>Build Your Bet</span>
                     <span>Betslip</span>
-                    {/* <select>
-                        {playerOptions}
-                    </select> */}
+
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
                     <button>STAT</button>
                     <button>H2H</button>
                     <button>TRIOS</button>
+                </div>
+                <div>
+                    <span>Add Player</span>
+                    <select style={{ width: '90%', margin: '0 10px' }}>
+                        {playerOptions}
+                    </select>
+                </div>
+                <div>
+                    <p>Select Statistic</p>
+                    <select style={{ width: '90%', margin: '0 10px' }}>
+
+                    </select>
                 </div>
                 <p>will have</p>
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -46,15 +56,22 @@ export const PropBets = props => {
                     <button>OVER</button>
                     <button>UNDER</button>
                 </div>
-                <div style={{ border: '1px solid black', textAlign: 'center', width: '35%', marginTop: '25px' }} >
-                    <p style={{ borderBottom: '1px solid black', paddingBottom: '10px' }}>{count}</p>
-                    <button style={{ width: '50%' }} onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}>
-                        -
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div style={{ border: '1px solid black', textAlign: 'center', width: '35%', margin: '25px 10px 10px 10px' }} >
+                        <p style={{ borderBottom: '1px solid black', paddingBottom: '10px' }}>{count}</p>
+                        <button style={{ width: '50%' }} onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}>
+                            -
                     </button>
-                    <button style={{ width: '50%' }} onClick={() => setCount(count + 1)}>
-                        +
+                        <button style={{ width: '50%' }} onClick={() => setCount(count + 1)}>
+                            +
                     </button>
 
+                    </div>
+                    <div style={{ border: '1px solid black', textAlign: 'center', width: '35%', margin: '25px 10px 10px 10px' }} >
+                        <p>{count}</p>
+                        <p>ODDS</p>
+
+                    </div>
                 </div>
             </div>
         )
