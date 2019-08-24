5c6d5cf6b15b47c0c410d74e6074497c7127c71f
Brandon Allison <brandona8128@outlook.com>
2019-08-23 23:28:24 -0400

    mergin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { Link, Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.scss';
 import Logo from './assets/logo.png'
 
-import PropBets from './components/PropBets/PropBets'
+import { PropBets } from './components/PropBets/PropBets'
 import NavBar from './components/NavBar'
 import { NFL } from "./components"
-<<<<<<< HEAD
+
 import SideBar from './components/SideBar'
-=======
+
 import CardContainer from './components/CardContainer'
 
 import styled from 'styled-components'
@@ -29,22 +29,18 @@ max-width: 100%;
 `
 
 const NavLinks = styled.div`
-
     a {
       padding-right: 50px;
       font-size: 2rem;
       color: ${colors.darkGrey};
       text-decoration: none;
-
       :hover {
         color: ${colors.primary}
       }
-
       :active {
         color: ${colors.primary}
       }
     }
-
 `
 
 const NavRow = styled.div`
@@ -61,7 +57,6 @@ const PageContent = styled.div`
 
 
 
->>>>>>> d4a0ecc71672b6eec9d70b4eac27807b265e2962
 function App() {
 
   // const [result, setResult] = useState([]);
@@ -74,20 +69,6 @@ function App() {
     <div className="App">
       <GlobalStyle />
 
-<<<<<<< HEAD
-      <NavBar />
-      {/* <SideBar /> */}
-      <header className="navbar">
-        <Link to="/"><h1>SportsBook</h1></Link>
-        <nav>
-          <Link onClick={() => { setSport('nfl') }}>NFL</Link>
-          <Link onClick={() => { setSport('mlb') }}>MLB</Link>
-          <Link onClick={() => { setSport('ncaa') }}>NCAA</Link>
-          <Link onClick={() => { setSport('cfl') }}>CFL</Link>
-          <Link onClick={() => { setSport('nba') }}>NBA</Link>
-        </nav>
-      </header>
-=======
       <NavContainer>
         <NavRow>
           <Link to="/">
@@ -106,23 +87,14 @@ function App() {
         </NavLinks>
       </NavContainer>
       <CardContainer />
->>>>>>> d4a0ecc71672b6eec9d70b4eac27807b265e2962
 
       <main>
         <NFL sport={sport} />
 
       </main>
-<<<<<<< HEAD
-      <div>
-
-        <PropBets />
-
-      </div>
-=======
       <PageContent>
         <PropBets />
       </PageContent>
->>>>>>> 3ebf0d09a341d58eef677882c329292f7df9fe65
 
     </div>
   )
--- src/components/PropBets/PropBets.js
+++ src/components/PropBets/PropBets.js
@@ -1,18 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Switch, Route, NavLink } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-import Stats from './Views/Stats'
-import HeadToHead from './Views/HeadToHead'
-import Trios from './Views/Trios'
-import InfoModal from './InfoModal'
 import axios from 'axios';
 
-<<<<<<< HEAD
-import './PropBets.scss'
-
-const PropBets = props => {
-=======
 import styled from 'styled-components'
 import { colors, buttonSize } from '../../theme/variables.js'
 
@@ -26,13 +14,13 @@ const Button = styled.button`
 const PropBetsContainer = styled.button`
     background: #fff;
     color: ${colors.darkGrey};
-    border: 1px solid black
+    border: 1px solid black;
     padding: 1.5rem;
 ${'' /* style={{ border: '1px solid black', height: '400px', width: '300px' }} */}
 `
 
 const PropBetsHeader = styled.button`
-    background: ${colors.darkGrey}
+    background: ${colors.darkGrey};
     color: #fff;
     border: 1px solid black;
     ${'' /* padding: 1.5rem; */}
@@ -41,18 +29,8 @@ ${'' /* style={{ border: '1px solid black', height: '400px', width: '300px' }} *
 `
 
 export const PropBets = props => {
->>>>>>> d4a0ecc71672b6eec9d70b4eac27807b265e2962
     const [players, setPlayers] = useState()
-    const [show, setShow] = useState(false)
-
-    const showModal = () => {
-        setShow(true);
-    };
-
-    const hideModal = () => {
-        setShow(false);
-    };
-
+    const [count, setCount] = useState(0);
 
     function getPlayers() {
         axios
@@ -70,35 +48,6 @@ export const PropBets = props => {
     let playerOptions = players && players.map((player) =>
         <option key={player.PlayerID}>{player.Name}</option>
     );
-<<<<<<< HEAD
-
-    // if (!players) return <h1>Loading...</h1>
-
-    return (
-
-        <div style={{
-            border: '1px solid black',
-            height: '600px',
-            width: '350px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between'
-        }}>
-            <div className='upper' id='upper'>
-
-                <div style={{ display: 'flex', justifyContent: 'space-between', padding: '5px' }}>
-                    <span>
-                        Build Your Bet <FontAwesomeIcon
-                            onMouseEnter={showModal}
-                            onMouseLeave={hideModal}
-                            icon={faInfoCircle}
-                        />
-                        <InfoModal
-                            show={show}
-                            handleClose={hideModal}
-                        />
-                    </span>
-=======
     if (!players) {
         return <h1>Loading...</h1>
     } else {
@@ -106,16 +55,10 @@ export const PropBets = props => {
             <PropBetsContainer>
                 <PropBetsHeader>
                     <span>Build Your Bet</span>
->>>>>>> 3ebf0d09a341d58eef677882c329292f7df9fe65
                     <span>Betslip</span>
 
                 </PropBetsHeader>
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-<<<<<<< HEAD
-                    <NavLink className='links' activeClass='active' to='/stats'><button>STAT</button></NavLink>
-                    <NavLink className='links' to='/headtohead'><button>H2H</button></NavLink>
-                    <NavLink className='links' to='/trios'><button>TRIOS</button></NavLink>
-=======
                     <Button primary small>STAT</Button>
                     <Button primary small>H2H</Button>
                     <Button primary small>TRIOS</Button>
@@ -154,34 +97,12 @@ export const PropBets = props => {
                         <p>ODDS</p>
 
                     </div>
->>>>>>> d4a0ecc71672b6eec9d70b4eac27807b265e2962
                 </div>
-<<<<<<< HEAD
-            </div>
-=======
             </PropBetsContainer>
         )
->>>>>>> 3ebf0d09a341d58eef677882c329292f7df9fe65
 
-            <div className='main'>
+    }
 
-                <Switch>
-                    <Route exact path='/stats' render={props => <Stats playerOptions={playerOptions} />} />
-                    <Route exact path='/headtohead' component={HeadToHead} />
-                    <Route exact path='/trios' component={Trios} />
-                </Switch>
 
-            </div>
-
-            <div className='lower'>
-                <p>Clear</p>
-                <button>Add To Betslip</button>
-            </div>
-
-        </div>
-    )
 
 }
-
-export default PropBets
-
