e29f1ad68a4b33bf16d6765f9c035a5d48163fc5
Brandon Allison <brandona8128@outlook.com>
2019-08-26 15:08:44 -0400

    fixed head to head

--- src/components/PropBets/Views/HeadToHead.js
+++ src/components/PropBets/Views/HeadToHead.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faChartLine } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faChartLine, faSyncAlt } from '@fortawesome/free-solid-svg-icons'
 import Select from 'react-select'
 
-import { statTypeOption } from '../../../assets/dummyData'
-import { Selection, Descriptor, Flex, StyledButton, CountDisplay } from '../styledComponents'
+import { statOptions, playerOptions, statTypeOption } from '../../../assets/dummyData'
+import { StyledButton, Descriptor, Flex, CountDisplay, CountSet } from '../styledComponents'
 
-const HeadToHead = _ => {
+const HeadToHead = props => {
+    console.log('player props', props)
     const [count, setCount] = useState(0)
-    const [statType, setStatType] = useState(null)
     const [selectedOption, setSelectedOption] = useState(null)
+    const [statType, setStatType] = useState(null)
     const [playerSelectedOption, setPlayerSelectedOption] = useState(null)
     const [betSlip, setBetSlip] = useState({})
+    const [prop, setProp] = useState('AT_LEAST')
+
+    const handleChange = selectedOption => {
+        setSelectedOption(selectedOption)
+        console.log(`Option selected:`, selectedOption)
+    }
+
+    const handleChangePlayer = playerSelectedOption => {
+        setPlayerSelectedOption(playerSelectedOption)
+        console.log(`Option selected:`, playerSelectedOption)
+    }
 
     const handleStatTypeChange = type => setStatType(type)
 
@@ -26,47 +38,85 @@ const HeadToHead = _ => {
         setPlayerSelectedOption(null)
     }
 
+    const stat = <FontAwesomeIcon icon={faChartLine} />
+    const player = <FontAwesomeIcon icon={faUser} />
+    const rotate = <FontAwesomeIcon icon={faSyncAlt} />
+
     return (
 
-        <>
+        <div style={{ display: 'flex' }}>
+            <div style={{ padding: '1rem', alignSelf: 'center' }}>
+                <p >{rotate}</p>
+            </div>
+            <div>
+                <div style={{ margin: '2rem 0', width: '100%', display: 'flex', flexDirection: 'column' }}>
+                    <Select
+                        className='select-player'
+                        value={playerSelectedOption}
+                        onChange={handleChangePlayer}
+                        options={playerOptions}
+                        isSearchable={true}
+                        placeholder={player}
+                    />
+                </div>
 
-            <Selection>
-                <FontAwesomeIcon icon={faUser} />
-                <p style={{ color: 'white', marginLeft: '10px' }}>SELECT PLAYER</p>
-            </Selection>
+                <Descriptor>Will Have More</Descriptor>
 
-            <Descriptor>Will have more</Descriptor>
+                <div style={{ margin: '2rem 0' }}>
 
-            <Selection>
-                <FontAwesomeIcon icon={faChartLine} />
-                <p style={{ color: 'white', marginLeft: '10px' }}>SELECT STAT</p>
-            </Selection>
 
-            <Descriptor>Than</Descriptor>
+                    <Select
+                        className='select-stat'
+                        value={selectedOption}
+                        onChange={handleChange}
+                        options={statOptions}
+                        isSearchable={true}
+                        placeholder={stat}
+                    />
 
-            <Selection>
-                <FontAwesomeIcon icon={faUser} />
-                <p style={{ color: 'white', marginLeft: '10px' }}>SELECT PLAYER</p>
-            </Selection>
 
-            <CountDisplay>
-                <p>{count}</p>
-                <Select
-                    value={statType}
-                    onChange={handleStatTypeChange}
-                    options={statTypeOption}
-                    isSearchable={true}
-                    defaultValue={statTypeOption[0].value}
-                    placeholder={statTypeOption[0].label}
-                />
-            </CountDisplay>
+                </div>
 
-            <Flex spaceAJ>
-                <StyledButton primary onClick={clearOptions}>Clear</StyledButton>
-                <StyledButton primary onClick={betSlipUpdate}>Send To Betslip</StyledButton>
-            </Flex>
+                <Descriptor> Than</Descriptor>
 
-        </>
+
+
+                <div style={{ margin: '2rem 0' }}>
+
+                    <Select
+                        className='select-player'
+                        value={playerSelectedOption}
+                        onChange={handleChangePlayer}
+                        options={playerOptions}
+                        isSearchable={true}
+                        placeholder={player}
+                    />
+
+                </div>
+            </div>
+
+            {/* <Flex spaceAJ>
+
+                <CountDisplay>
+                    <p>{count}</p>
+                    <Select
+                        value={statType}
+                        onChange={handleStatTypeChange}
+                        options={statTypeOption}
+                        isSearchable={true}
+                        defaultValue={statTypeOption[0].value}
+                        placeholder={statTypeOption[0].label}
+                    />
+                </CountDisplay>
+            </Flex> */}
+
+
+            {/* <Flex spaceAJ>
+                    <StyledButton primary onClick={clearOptions}>Clear</StyledButton>
+                    <StyledButton primary onClick={betSlipUpdate}>Send To Betslip</StyledButton>
+                </Flex> */}
+
+        </div>
 
     )
 
