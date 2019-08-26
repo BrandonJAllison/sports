e09e21f8efa3fecc00a3a477dff6793ed773d5ee
brellin <cekakid@gmail.com>
2019-08-25 21:17:57 -0500

    remove subcomponent

--- src/components/PropBets/Views/Stats.js
+++ src/components/PropBets/Views/Stats.js
@@ -3,18 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faChartLine } from '@fortawesome/free-solid-svg-icons'
 import Select from 'react-select'
 
-import { statOptions, playerOptions } from '../../../assets/dummyData'
-import { StyledButton, Descriptor } from '../styledComponents'
-import Count from './subComponents/Count'
+import { statOptions, playerOptions, statTypeOption } from '../../../assets/dummyData'
+import { StyledButton, Descriptor, Flex, CountDisplay, CountSet } from '../styledComponents'
 
-const Stats = (props) => {
+const Stats = props => {
     console.log('player props', props)
     const [count, setCount] = useState(0)
     const [selectedOption, setSelectedOption] = useState(null)
     const [statType, setStatType] = useState(null)
     const [playerSelectedOption, setPlayerSelectedOption] = useState(null)
     const [betSlip, setBetSlip] = useState({})
-    const [prop, setProp] = useState('~')
+    const [prop, setProp] = useState('AT_LEAST')
 
     const handleChange = selectedOption => {
         setSelectedOption(selectedOption)
@@ -59,6 +58,48 @@ const Stats = (props) => {
 
             <Descriptor>Will Have</Descriptor>
 
+            <div>
+                <StyledButton
+                    third small primary first
+                    active={prop === 'AT_LEAST'}
+                    onClick={() => setProp('AT_LEAST')}
+                >AT LEAST</StyledButton>
+                <StyledButton
+                    third small primary
+                    active={prop === 'OVER'}
+                    onClick={() => setProp('OVER')}
+                >OVER</StyledButton>
+                <StyledButton
+                    third small primary last
+                    active={prop === 'UNDER'}
+                    onClick={() => setProp('UNDER')}
+                >UNDER</StyledButton>
+            </div>
+
+            <Flex spaceAJ>
+                <CountSet>
+                    <p>{count}</p>
+                    <StyledButton
+                        onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}
+                        half small>-</StyledButton>
+                    <StyledButton
+                        onClick={() => setCount(count + 1)}
+                        half small>+</StyledButton>
+                </CountSet>
+
+                <CountDisplay>
+                    <p>{count}</p>
+                    <Select
+                        value={statType}
+                        onChange={handleStatTypeChange}
+                        options={statTypeOption}
+                        isSearchable={true}
+                        defaultValue={statTypeOption[0].value}
+                        placeholder={statTypeOption[0].label}
+                    />
+                </CountDisplay>
+            </Flex>
+
             <div style={{ margin: '2rem 0' }}>
                 <Select
                     className='select-stat'
@@ -70,32 +111,10 @@ const Stats = (props) => {
                 />
             </div>
 
-            <div>
-                <StyledButton
-                    third small primary first
-                    active={prop === '~'}
-                    onClick={() => setProp('~')}
-                >AT LEAST</StyledButton>
-                <StyledButton
-                    third small primary
-                    active={prop === '+'}
-                    onClick={() => setProp('+')}
-                >OVER</StyledButton>
-                <StyledButton
-                    third small primary last
-                    active={prop === '-'}
-                    onClick={() => setProp('-')}
-                >UNDER</StyledButton>
-            </div>
-
-            <Count
-                count={count}
-                setCount={setCount}
-                statType={statType}
-                handleStatTypeChange={handleStatTypeChange}
-                clearOptions={clearOptions}
-                betSlipUpdate={betSlipUpdate}
-            />
+            <Flex spaceAJ>
+                <StyledButton primary onClick={clearOptions}>Clear</StyledButton>
+                <StyledButton primary onClick={betSlipUpdate}>Send To Betslip</StyledButton>
+            </Flex>
 
         </>
 
