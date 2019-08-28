620c588878b4785379c53dd58c75a9448f6d74b8
Brandon Allison <brandona8128@outlook.com>
2019-08-27 21:48:47 -0400

    same shit different day

--- src/assets/urls.js
+++ src/assets/urls.js
@@ -3,11 +3,6 @@ export default {
     MLB: 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4424',
     NCAA: 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4479',
     CFL: 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4405',
-<<<<<<< HEAD
-    NBA: 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4387'
-}
-=======
     NBA: 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4387',
     players: 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_players.php?id='
 }
->>>>>>> 8fc05a07bf245da75cdf2f9febe1299506ff5612
